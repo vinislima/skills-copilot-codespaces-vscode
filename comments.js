3727bcb1a2dc6f9752e81843ff8718f4c1b37d52
Vinícius Lima <vinislima@gmail.com>
2024-05-28 21:06:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,14 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    fs.readFile('index.html', (err, data) => {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
 });
 
 server.listen(port, hostname, () => {
